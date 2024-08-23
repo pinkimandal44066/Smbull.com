c528b5f24c8d65ab0e4cf426556eb8b2a9001dc3
Pinki Kumari <160412282+pinkimandal44066@users.noreply.github.com>
2024-08-24 00:38:30 +0530

    home

---
+++ index.js
@@ -0,0 +1,6 @@
+const header = document.querySelector("header");
+const hamburgerBtn = document.querySelector("#hamburger-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+
+hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
+closeMenuBtn.addEventListener("click", () => header.classList.remove("show-mobile-menu"));
